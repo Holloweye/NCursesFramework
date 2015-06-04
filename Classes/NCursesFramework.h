6e2d3c1f8c6efa04bac8a73d0c8ebc8aea890ccb
Christer Ulfsparre Lyremar <christer@dynamo.mobi>
2015-06-04 16:55:56 +0200

    Added documentation.

---
+++ Classes/NCursesFramework.h
@@ -0,0 +1,37 @@
+//
+//  NCursesFramework.h
+//  NCursesFramework
+//
+//  Created by Christer Ulfsparre on 04/06/15.
+//  Copyright (c) 2015 ChristerUL. All rights reserved.
+//
+
+#ifndef NCursesFramework_NCursesFramework_h
+#define NCursesFramework_NCursesFramework_h
+
+#import "NCMutableString.h"
+#import "NCTextEdit.h"
+#import "NCLinearLayout.h"
+#import "NCRelativeLayout.h"
+#import "NCSpacer.h"
+#import "NCLayoutInflator.h"
+#import "NCDummyPlatform.h"
+#import "NCDummyRendition.h"
+#import "NCString.h"
+#import "NCKey.h"
+#import "NCRendition.h"
+#import "NCCursesRendition.h"
+#import "Logger.h"
+#import "NCCanvas.h"
+#import "NCBorderDecorator.h"
+#import "NCBorderDottedStrategy.h"
+#import "NCBorderFilledStrategy.h"
+#import "NCBorderStrategy.h"
+#import "NCText.h"
+#import "NCGraphic.h"
+#import "NCCursesPlatform.h"
+#import "NCPlatform.h"
+#import "NCMatrix.h"
+#import "NCScrollDecorator.h"
+
+#endif
